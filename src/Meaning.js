1d31413f1fa14a81a9fa2aaef3422bdb54d10f7b
Fiorlore <87369031+Fiorlore@users.noreply.github.com>
2022-02-16 15:31:07 +0100

    initial commit

---
+++ src/Meaning.js
@@ -0,0 +1,26 @@
+import Synonyms from "./Synonyms";
+
+export default function Meaning(props) {
+    console.log(props.meaning);
+
+    return (
+        <div className="Meaning">
+        <h3>{props.meaning.partOfSpeech}</h3>
+
+        {props.meaning.definitions.map(function(definition, index) {
+            return (
+                <div key={index}>
+                    <p>
+                        {definition.definition}
+                        <br />
+                        <em>{definition.example}</em>
+                        <br />
+                        <Synonyms synonyms={definition.synonyms} />
+                    </p>
+                </div>
+            );
+        })}
+
+        </div>
+    );
+}
