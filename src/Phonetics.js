9c399cb9c2e5a37d764d5fd5e5789ac8ddb764d3
Fiorlore <87369031+Fiorlore@users.noreply.github.com>
2022-02-18 13:08:28 +0100

    updated to match the new api

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -5,7 +5,7 @@ export default function Phonetics(props) {
     return (
         <div className="Phonetics">
             <a href={props.phonetics[0].audio} target="_blank" rel="noopener noreferrer"> &#9654; </a>
-            /{props.phonetics[0].text}/
+            {props.phonetics[0].text}
         </div>
     );
     
