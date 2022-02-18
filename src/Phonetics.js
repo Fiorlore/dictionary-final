837e37bfdd8415cf3425c50c5ec663b3df1c578d
Fiorlore <87369031+Fiorlore@users.noreply.github.com>
2022-02-18 14:49:50 +0100

    updated not to show phonetics if not available

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,6 +2,7 @@ import "./Phonetics.css";
 
 export default function Phonetics(props) {
     
+    if (props.phonetics[0].audio) {
     return (
         <div className="Phonetics">
             <a href={props.phonetics[0].audio} target="_blank" rel="noopener noreferrer"> &#9654; </a>
@@ -9,5 +10,12 @@ export default function Phonetics(props) {
         </div>
     );
     
-
+    } else {
+        return (
+            <div className="Phonetics">
+               
+                {props.phonetics[0].text}
+            </div>
+        );
+    }
 }
